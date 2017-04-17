8234a5da21c5365b106fde56ba47feda3ea6a530
Jacob <jacob.varner14@gmail.com>
2017-04-17 14:37:58 -0500

    Uploaded server.py file for API code.

---
+++ API/server.py
@@ -0,0 +1,14 @@
+import random
+def lambda_handler(event, context):
+    random.seed()
+    itemno = random.randint(0, 3)
+    if itemno == 0:
+        values = "Your claim has been approved."
+    elif itemno == 1:
+        values = "Unum does not have a claim application on file for you, please contact customer support at 555-555-5555."
+    elif itemno == 2:
+        values = "Your claim is currently being processed."
+    elif itemno == 3:
+        values = "Your claim has been processed. Please contact customer service at 555-555-5555 for more information."
+    json = {"version": "string", "response": {"outputSpeech": {"type": "PlainText","text": values},"card":{"type": "Simple","title": "Unum","content": values},"shouldEndSession": 'true'}}
+    return json
