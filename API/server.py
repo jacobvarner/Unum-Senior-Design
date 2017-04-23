54ea0e68bcb470834dacb83f1b270896148dd649
pittman-tyler <pittman.tyler@live.com>
2017-04-23 16:02:26 -0500

    Created logic for the server to differentiate between devices

--- API/server.py
+++ API/server.py
@@ -1,15 +1,33 @@
 import random
+import re
+
 def lambda_handler(event, context):
+    try:
+        if('querystring' in event):
+            eventParms = event['querystring'].strip('{}').replace(',',' ')
+            eventDict = dict(re.findall(r'(\S+)=(".*?"|\S+)', eventParms))
+            returnValue = generateText(eventParms)
+            return returnValue
+        else:
+            return u"querystring not present\n"
+    except:
+        return u"failed"
+
+def generateText(values):
     #TODO: Insert parsing of GET request. Use information from Process assigmnent
     random.seed()
     itemno = random.randint(0, 3)
     if itemno == 0:
-        values = "Your claim has been approved."
+        ret = "Your claim has been approved."
     elif itemno == 1:
-        values = "Unum does not have a claim application on file for you, please contact customer support at 555-555-5555."
+        ret = "Unum does not have a claim application on file for you, please contact customer support at 555-555-5555."
     elif itemno == 2:
-        values = "Your claim is currently being processed."
+        ret = "Your claim is currently being processed."
     elif itemno == 3:
-        values = "Your claim has been processed. Please contact customer service at 555-555-5555 for more information."
-    json = {"version": "string", "response": {"outputSpeech": {"type": "PlainText","text": values},"card":{"type": "Simple","title": "Unum","content": values},"shouldEndSession": 'true'}}
+        ret = "Your claim has been processed. Please contact customer service at 555-555-5555 for more information."
+    json = ""
+    if('Alexa' in values):
+        json = {"version": "string", "response": {"outputSpeech": {"type": "PlainText","text": ret},"card":{"type": "Simple","title": "Unum","content": ret},"shouldEndSession": 'true'}}
+    if('Google' in values):
+        json = {"followupEvent":{},"contextOut":[],"data":{},"type":0,"speech":ret,"source":"apiai-weather-webhook-sample","displayText":ret}
     return json
